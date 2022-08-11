e8045536be7a19cbccba057d17fbe824cfe75995
maciejpieczarka <63975664+maciejpieczarka@users.noreply.github.com>
2022-08-11 15:45:58 +0200

    Add version 1 files

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='greetings'>
+      <h1>React / Typescript / Parcel </h1>
+      <h2>App Template v1.0.0</h2>
+      <p>Created by Maciej Pieczarka &copy;2022</p>
+      <a href='https://github.com/maciejpieczarka' target='_blank' rel='noreferrer noopener'>
+        Check out my github
+      </a>
+    </div>
+  );
+};
+
+export default App;
